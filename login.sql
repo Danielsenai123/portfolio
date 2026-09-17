027fc5e0f23677a09f1c1bab13c1c01118c36927
daniel <daniel.l.lopes7@aluno.senai.br>
2026-09-17 18:19:14 -0300

    Deixa paginas responsivas

--- login.sql
+++ login.sql
@@ -4,23 +4,19 @@ COLLATE utf8mb4_unicode_ci;
 
 USE sistema_login;
 
-
 SET FOREIGN_KEY_CHECKS = 0;
 
-
 DROP TABLE IF EXISTS contas_sociais, recuperacao_senha, tokens_sessao, clientes, usuarios;
-
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    tipo_usuario ENUM('aluno', 'professor', 'admin') NOT NULL DEFAULT 'aluno',
+    tipo_usuario ENUM('aluno', 'professor', 'admin', 'ceo') NOT NULL DEFAULT 'aluno',
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
 
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -30,8 +26,6 @@ CREATE TABLE clientes (
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
-
 
 CREATE TABLE contas_sociais (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -62,21 +56,32 @@ CREATE TABLE tokens_sessao (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
-
 SET FOREIGN_KEY_CHECKS = 1;
 
+INSERT INTO usuarios (nome, email, senha, tipo_usuario) 
+VALUES (
+    'Administrador',
+    'admin@email.com',
+    '$2b$10$Gd43KpT3TqXfADMkgJzA0eNx4mZndtNiKebKlZ/9kedHbQTBNOM7O',
+    'admin'
+);
 
 INSERT INTO usuarios (nome, email, senha, tipo_usuario) 
-VALUES ('Administrador', 'admin@email.com', '$2b$10$Gd43KpT3TqXfADMkgJzA0eNx4mZndtNiKebKlZ/9kedHbQTBNOM7O', 'admin');
-
+VALUES (
+    'Professores',
+    'professores@sesisenai.com',
+    '$2b$10$tIuGXFrsRqTMuqu7zVri1uvz892EdX3LC3Uu3uukaGL31rRQAYK/u',
+    'professor'
+);
 
 INSERT INTO usuarios (nome, email, senha, tipo_usuario) 
-VALUES ('Professor Carlos', 'carlos@escola.com', '$2b$10$tIuGXFrsRqTMuqu7zVri1uvz892EdX3LC3Uu3uukaGL31rRQAYK/u', 'professor');
-
-
-INSERT INTO usuarios (nome, email, senha, tipo_usuario) 
-VALUES ('Aluno João', 'joao@email.com', '$2b$10$tIuGXFrsRqTMuqu7zVri1uvz892EdX3LC3Uu3uukaGL31rRQAYK/u', 'aluno');
-
+VALUES (
+    'Daniel Leal',
+    'DanielCeo@gmail.com',
+    '$2b$10$tIuGXFrsRqTMuqu7zVri1uvz892EdX3LC3Uu3uukaGL31rRQAYK/u',
+    'ceo'
+);
 
 SHOW TABLES;
+
 SELECT * FROM usuarios;
